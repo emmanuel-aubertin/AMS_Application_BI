ca236fd23ebefb03b35d29ea150c3f9cd7a86ceb
Emmanuel <37215934+emmanuel-aubertin@users.noreply.github.com>
2024-10-02 13:47:09 +0200

    😎 Parse float file  😎

--- src/parser.cpp
+++ src/parser.cpp
@@ -5,9 +5,9 @@
 
 Parser::Parser(char delimiter) : m_delimiter(delimiter) {}
 
-std::vector<int> Parser::split(const std::string &s)
+std::vector<u_int16_t> Parser::split(const std::string &s)
 {
-    std::vector<int> tokens;
+    std::vector<u_int16_t> tokens;
     std::string token;
     std::istringstream tokenStream(s);
     while (std::getline(tokenStream, token, m_delimiter))
@@ -17,9 +17,21 @@ std::vector<int> Parser::split(const std::string &s)
     return tokens;
 }
 
+std::vector<float> Parser::splitFloat(const std::string &s)
+{
+    std::vector<float> tokens;
+    std::string token;
+    std::istringstream tokenStream(s);
+    while (std::getline(tokenStream, token, m_delimiter))
+    {
+        tokens.push_back(std::stof(token));
+    }
+    return tokens;
+}
+
 void Parser::parseFile(const std::string &filename)
 {
-    std::vector<std::vector<int>> result;
+    std::vector<std::vector<u_int16_t>> result;
     std::string line;
     std::ifstream file(filename);
     if (file.is_open())
@@ -31,6 +43,22 @@ void Parser::parseFile(const std::string &filename)
         file.close();
     }
     parsedFile = result;
+}
+
+void Parser::parseWeightFile(const std::string &filename)
+{
+    std::vector<std::vector<float>> result;
+    std::string line;
+    std::ifstream file(filename);
+    if (file.is_open())
+    {
+        while (getline(file, line))
+        {
+            result.push_back(splitFloat(line));
+        }
+        file.close();
+    }
+    parsedWeightFile = result;
 }
 
 void Parser::print() const
@@ -49,7 +77,7 @@ void Parser::print() const
     }
 }
 
-std::vector<std::vector<int>> Parser::getParsedFile()
+std::vector<std::vector<u_int16_t>> Parser::getParsedFile()
 {
     return parsedFile;
 }
--- src/parser.hpp
+++ src/parser.hpp
@@ -8,14 +8,18 @@ class Parser
 {
 public:
     Parser(char delimiter = ',');
-    std::vector<int> split(const std::string &s);
+    std::vector<u_int16_t> split(const std::string &s);
+    std::vector<float> splitFloat(const std::string &s);
+    
     void parseFile(const std::string &filename);
     void print() const;
-    std::vector<std::vector<int>> getParsedFile();
+    std::vector<std::vector<u_int16_t>> getParsedFile();
+    void parseWeightFile(const std::string &filename);
 
 private:
     char m_delimiter;
-    std::vector<std::vector<int>> parsedFile;
+    std::vector<std::vector<u_int16_t>> parsedFile;
+    std::vector<std::vector<float>> parsedWeightFile;
 };
 
 #endif // PARSER_H
