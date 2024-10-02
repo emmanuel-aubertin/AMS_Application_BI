c32afe8fcef636230535812d028cff6122231cbc
Emmanuel <37215934+emmanuel-aubertin@users.noreply.github.com>
2024-10-02 11:25:46 +0200

    🙃 Updated parser with print and attribute 🙃

--- src/parser.cpp
+++ src/parser.cpp
@@ -1,28 +1,55 @@
 #include "parser.hpp"
+#include <iostream>
 #include <fstream>
 #include <sstream>
 
 Parser::Parser(char delimiter) : m_delimiter(delimiter) {}
 
-std::vector<int> Parser::split(const std::string& s) const {
+std::vector<int> Parser::split(const std::string &s)
+{
     std::vector<int> tokens;
     std::string token;
     std::istringstream tokenStream(s);
-    while (std::getline(tokenStream, token, m_delimiter)) {
+    while (std::getline(tokenStream, token, m_delimiter))
+    {
         tokens.push_back(std::stoi(token));
     }
     return tokens;
 }
 
-std::vector<std::vector<int>> Parser::parseFile(const std::string& filename) const {
+void Parser::parseFile(const std::string &filename)
+{
     std::vector<std::vector<int>> result;
     std::string line;
     std::ifstream file(filename);
-    if (file.is_open()) {
-        while (getline(file, line)) {
+    if (file.is_open())
+    {
+        while (getline(file, line))
+        {
             result.push_back(split(line));
         }
         file.close();
     }
-    return result;
+    parsedFile = result;
+}
+
+void Parser::print() const
+{
+    for (const auto &row : parsedFile)
+    {
+        for (size_t i = 0; i < row.size(); ++i)
+        {
+            std::cout << row[i];
+            if (i != row.size() - 1)
+            {
+                std::cout << ", ";
+            }
+        }
+        std::cout << std::endl;
+    }
+}
+
+std::vector<std::vector<int>> Parser::getParsedFile()
+{
+    return parsedFile;
 }
--- src/parser.hpp
+++ src/parser.hpp
@@ -1,17 +1,21 @@
-#ifndef PARSER_H
-#define PARSER_H
+#ifndef PARSER_HPP
+#define PARSER_HPP
 
 #include <string>
 #include <vector>
 
-class Parser {
+class Parser
+{
 public:
     Parser(char delimiter = ',');
-    std::vector<int> split(const std::string& s) const;
-    std::vector<std::vector<int>> parseFile(const std::string& filename) const;
+    std::vector<int> split(const std::string &s);
+    void parseFile(const std::string &filename);
+    void print() const;
+    std::vector<std::vector<int>> getParsedFile();
 
 private:
     char m_delimiter;
+    std::vector<std::vector<int>> parsedFile;
 };
 
 #endif // PARSER_H
